3df77bcb6c69232361e88ef06b326edbe66b5a40
Revadike <revadike@outlook.com>
2021-05-02 02:49:27 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,35 @@
+const RevaPlugin = require("@revaplugin/RevaPlugin");
+const { createConnection } = require("typeorm");
+
+module.exports = class Database extends RevaPlugin {
+
+    constructor() {
+        super();
+        this.connection = null;
+        // this.loadTasks();
+    }
+
+    async start() {
+        let options = {
+            "type":     "sqlite",
+            "database": "./data/local.sqlite",
+            "logging":  true,
+        };
+
+        this.connection = await createConnection(options);
+        // let repo = connection.getRepository();
+
+        // switch to user configure database engine
+    }
+
+    getInstanceSettings(name) {
+        if (!this.connection) {
+            throw new Error("Connect to the database first");
+        }
+
+        // retrieve plugin settings from db
+
+        return { name };
+    }
+
+};
